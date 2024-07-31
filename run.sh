247c5de86ba8a9bbbc49b5713f43564624c58487
Piotr Napierala <piotr.napierala94@gmail.com>
2024-07-31 10:42:24 +0200

    change start command

--- run.sh
+++ run.sh
@@ -1,16 +1,8 @@
 #!/bin/bash -e
 
-echo "Starting database migration..."
 pushd ./drizzle/migrate
 npm run db:migrate
-popd
-echo "Database migration completed."
+popd 
 
-echo "Starting the server..."
 # node server.js
-npm run start
-if [ $? -ne 0 ]; then
-  echo "Server failed to start"
-  exit 1
-fi
-echo "Server has started."
+npm start
