e05a1eeb44a3cc54daed30dded9c6e8d37f9ca80
Piotr Napierala <piotr.napierala94@gmail.com>
2024-07-31 09:07:13 +0200

    added logging to run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,11 @@
 #!/bin/bash -e
 
+echo "Starting database migration..."
 pushd ./drizzle/migrate
 npm run db:migrate
-popd 
+popd
+echo "Database migration completed."
 
+echo "Starting the server..."
 node server.js
+echo "Server has started."
