4f3660d381704a758a9bce477d07244c99382253
Piotr Napierala <piotr.napierala94@gmail.com>
2024-08-01 12:58:05 +0200

    move cached profile loader outside of tsx file

--- src/app/dashboard/settings/profile/page.tsx
+++ src/app/dashboard/settings/profile/page.tsx
@@ -2,15 +2,24 @@ import { ProfileImage } from "@/app/dashboard/settings/profile/profile-image"
 import { ProfileName } from "@/app/dashboard/settings/profile/profile-name"
 import { EditBioForm } from "./edit-bio-form"
 import { getCurrentUser } from "@/lib/session"
-import { Suspense, cache } from "react"
-import { getUserProfileUseCase } from "@/use-cases/users"
+import { Suspense } from "react"
 import { Skeleton } from "@/components/ui/skeleton"
 import { ConfigurationPanel } from "@/components/configuration-panel"
-
-//@ts-ignore
-export const getUserProfileLoader = cache(getUserProfileUseCase)
+import { getUserProfileLoader } from "../../projects/[projectId]/posts/[postId]/loaders"
 
 export default async function SettingsPage() {
+  async function BioFormWrapper() {
+    const user = await getCurrentUser()
+
+    if (!user) {
+      throw new Error("User not found")
+    }
+
+    const profile = await getUserProfileLoader(user.id)
+
+    return <EditBioForm bio={profile.bio} />
+  }
+
   return (
     <div className="space-y-8">
       <div className="grid grid-cols-2 gap-8">
@@ -25,16 +34,4 @@ export default async function SettingsPage() {
       </ConfigurationPanel>
     </div>
   )
-}
-
-export async function BioFormWrapper() {
-  const user = await getCurrentUser()
-
-  if (!user) {
-    throw new Error("User not found")
-  }
-
-  const profile = await getUserProfileLoader(user.id)
-
-  return <EditBioForm bio={profile.bio} />
 }
