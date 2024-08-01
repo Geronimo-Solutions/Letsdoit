e9e485520d3b095ccf27c697e34f05ab1a6364d7
Piotr Napierala <piotr.napierala94@gmail.com>
2024-08-01 17:58:29 +0200

    update max free projects

--- src/app-config.tsx
+++ src/app-config.tsx
@@ -1,20 +1,20 @@
 export const appConfig: {
-  mode: "comingSoon" | "maintence" | "live";
+  mode: "comingSoon" | "maintence" | "live"
 } = {
   mode: "live",
-};
+}
 
-export const protectedRoutes = ["/purchases", "/dashboard"];
-export const applicationName = "Lets Do It";
-export const companyName = "Geronimo";
+export const protectedRoutes = ["/purchases", "/dashboard"]
+export const applicationName = "Lets Do It"
+export const companyName = "Geronimo"
 
-export const MAX_UPLOAD_IMAGE_SIZE_IN_MB = 5;
-export const MAX_UPLOAD_IMAGE_SIZE = 1024 * 1024 * MAX_UPLOAD_IMAGE_SIZE_IN_MB;
+export const MAX_UPLOAD_IMAGE_SIZE_IN_MB = 5
+export const MAX_UPLOAD_IMAGE_SIZE = 1024 * 1024 * MAX_UPLOAD_IMAGE_SIZE_IN_MB
 
-export const TOKEN_LENGTH = 32;
-export const TOKEN_TTL = 1000 * 60 * 5; // 5 min
-export const VERIFY_EMAIL_TTL = 1000 * 60 * 60 * 24 * 7; // 7 days
+export const TOKEN_LENGTH = 32
+export const TOKEN_TTL = 1000 * 60 * 5 // 5 min
+export const VERIFY_EMAIL_TTL = 1000 * 60 * 60 * 24 * 7 // 7 days
 
-export const MAX_PROJECT_LIMIT = 10;
+export const MAX_PROJECT_LIMIT = 5
 
-export const afterLoginUrl = "/dashboard";
+export const afterLoginUrl = "/dashboard"
