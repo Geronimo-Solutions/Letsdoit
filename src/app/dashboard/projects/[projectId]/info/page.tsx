a5085590a8f410e75fab0d804fdb595d666af276
Piotr Napierala <piotr.napierala94@gmail.com>
2024-08-01 17:52:10 +0200

    update project dashboard tiles on mobile

--- src/app/dashboard/projects/[projectId]/info/page.tsx
+++ src/app/dashboard/projects/[projectId]/info/page.tsx
@@ -1,41 +1,29 @@
-import { EditProjectInfoForm } from "./edit-project-info-form";
-import { getCurrentUser } from "@/lib/session";
+import { EditProjectInfoForm } from "./edit-project-info-form"
+import { getCurrentUser } from "@/lib/session"
 import {
   getPublicProjectInfoByIdUseCase,
   isAdminOrOwnerOfProjectUseCase,
-} from "@/use-cases/projects";
-import { getProjectImageUrl } from "../settings/util";
-import Image from "next/image";
-import {
-  DiscordIcon,
-  GithubIcon,
-  XIcon,
-  YoutubeIcon,
-} from "@/components/icons";
-import Link from "next/link";
-import { socialIconStyles } from "@/styles/icons";
-import { NotFoundError } from "@/app/util";
-import { cardStyles, pageTitleStyles } from "@/styles/common";
-import { cn } from "@/lib/utils";
+} from "@/use-cases/projects"
+import { getProjectImageUrl } from "../settings/util"
+import Image from "next/image"
+import { DiscordIcon, GithubIcon, XIcon, YoutubeIcon } from "@/components/icons"
+import Link from "next/link"
+import { socialIconStyles } from "@/styles/icons"
+import { NotFoundError } from "@/app/util"
+import { cardStyles, pageTitleStyles } from "@/styles/common"
+import { cn } from "@/lib/utils"
 
-export default async function InfoPage({
-  params,
-}: {
-  params: { projectId: string };
-}) {
-  const { projectId } = params;
+export default async function InfoPage({ params }: { params: { projectId: string } }) {
+  const { projectId } = params
 
-  const user = await getCurrentUser();
+  const user = await getCurrentUser()
 
-  const isAdminOrOwner = await isAdminOrOwnerOfProjectUseCase(
-    user,
-    parseInt(projectId)
-  );
+  const isAdminOrOwner = await isAdminOrOwnerOfProjectUseCase(user, parseInt(projectId))
 
-  const project = await getPublicProjectInfoByIdUseCase(parseInt(projectId));
+  const project = await getPublicProjectInfoByIdUseCase(parseInt(projectId))
 
   if (!project) {
-    throw new NotFoundError("Project not found");
+    throw new NotFoundError("Project not found")
   }
 
   return (
@@ -44,7 +32,7 @@ export default async function InfoPage({
         <div>Info</div>
       </h1>
 
-      <div className="flex gap-8">
+      <div className="flex flex-col-reverse md:flex-row gap-8">
         <div className="flex-grow">
           <EditProjectInfoForm
             isAdminOrOwner={isAdminOrOwner}
@@ -54,7 +42,7 @@ export default async function InfoPage({
         </div>
 
         <div
-          className={cn(cardStyles, "w-[300px] p-8 flex-shrink-0 space-y-8")}
+          className={cn(cardStyles, "w-full sm:w-[300px] p-8 sm:flex-shrink-0 space-y-8")}
         >
           <Image
             src={getProjectImageUrl(project)}
@@ -111,5 +99,5 @@ export default async function InfoPage({
         </div>
       </div>
     </div>
-  );
+  )
 }
