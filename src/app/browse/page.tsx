webdevcody/wdc-saas-starter-kit

use white text on dark background

src/app/browse/page.tsx
@@ -110,7 +110,7 @@ async function GroupsList({ search, page }: { search?: string; page: number }) {
           height="200"
           alt="no gruops placeholder image"
         ></Image>
-        <h2 className="text-2xl">No groups matching your search</h2>
+        <h2 className="text-2xl text-white">No groups matching your search</h2>
       </div>
     );
   }

Geronimo-Solutions/Letsdoit

src/app/browse/page.tsx
@@ -101,7 +101,8 @@ async function ProjectsList({ search, page }: { search?: string; page: number })
           height="200"
           alt="no gruops placeholder image"
         ></Image>
-        <h2 className="text-2xl">No projects matching your search</h2>
+
+        <h2 className="text-2xl text-white">No projects matching your search</h2>
       </div>
     )
   }