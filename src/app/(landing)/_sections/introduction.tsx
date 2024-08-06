f26d17c04e7807da0508d6bf8e7ebbc8cf290a1b
Piotr Napierala <piotr.napierala94@gmail.com>
2024-08-06 22:55:19 +0200

    fix intro section styling

--- src/app/(landing)/_sections/introduction.tsx
+++ src/app/(landing)/_sections/introduction.tsx
@@ -2,8 +2,8 @@ import Image from "next/image"
 
 export function IntroductionSection() {
   return (
-    <div className="flex flex-col items-start align-center md:flex-row gap-x-24 w-3/4 mx-auto my-16 bg-gray-200 dark:bg-background">
-      <section className="flex-1">
+    <div className="flex flex-col items-start align-center md:flex-row gap-x-24 w-full p-16 bg-gray-200 dark:bg-background">
+      <section className="w-full md:max-w-[500px] md:ml-auto">
         <div className="">
           <p className="text-4xl mb-12 max-w-4xl mx-auto text-left">
             Connect with
@@ -12,7 +12,7 @@ export function IntroductionSection() {
           </p>
         </div>
         <div className="">
-          <p className="mt-5 text-gray-400 text-lg">
+          <p className="mt-5 text-gray-600 dark:text-gray-400 text-lg">
             Our online service makes it easy to connect with others who share their ideas,
             whether it's tech, wellness, or hobby. Create or join a project, schedule
             meetups, and enjoy pursuing your dreams with new friends by your side. Start
@@ -20,9 +20,9 @@ export function IntroductionSection() {
           </p>
         </div>
       </section>
-      <section className="flex-1 pt-12 md:pt-0 mx-auto">
+      <section className="pt-12 md:pt-0 mx-auto md:mr-auto">
         <Image
-          className="rounded-xl md:size-[400px]"
+          className="rounded-xl md:h-[400px] md:w-[400px]"
           width="200"
           height="200"
           src="/project.jpeg"
